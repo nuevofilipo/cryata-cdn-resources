34a3c468fedb3dbd973043efadf8c13ef32baf0b
nuevofilipo <111537061+nuevofilipo@users.noreply.github.com>
2024-06-18 20:56:14 +0200

    Update main.js

--- table view/main.js
+++ table view/main.js
@@ -307,8 +307,10 @@ document
 //! html functionality
 function goToChartViewPage(coin) {
     // for this to work, the local host of the chart view page has to be open
+  console.log("clicked");
     var timeframe = document.querySelector(".tablinks.active").getAttribute("data-timeframe");
    window.location.href = `http://127.0.0.1:5500/frontend/html-files/chartViewPage.html?coin=${coin}&timeframe=${timeframe}`;
+  
 }
 
 let mybutton = document.getElementById("backToTopBtn");
